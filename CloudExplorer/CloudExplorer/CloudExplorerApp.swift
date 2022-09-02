0dfe7eaf5b36078526fae791ba084f13e103355f
Cristian Murariu <cristi.m.arnia@gmail.com>
2022-09-02 14:49:29 +0300

    Added template project

---
+++ CloudExplorer/CloudExplorer/CloudExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudExplorerApp.swift
+//  CloudExplorer
+//
+//  Created by Cristian Murariu on 02.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct CloudExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
